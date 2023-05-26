0375a0cc7466f21b0f7b9248a7bdf406ff010c19
SuperAlexcom <114802210+SuperAlexcom@users.noreply.github.com>
2023-05-26 22:00:30 +0300

    Update Orange.java

--- src/Orange.java
+++ src/Orange.java
@@ -1,7 +1,7 @@
 public class Orange extends BaseFruit {
     public Orange(Point position) {
         score =2;
-        imagePath ="";
+        imagePath ="resources/orange.png";
         this.position=position;
 
     }
