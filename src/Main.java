5c7fa277f49eb68e9272ff92441ccb060024d540
Artem Pasechnik <pasechnik@1060.ru>
2023-05-26 23:22:44 +0300

    fixed movement

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
         //Start window
         SnakeHead.setDimensions(18,16);
+        FruitFactory.setDimensions(new Point(18,16));
         Controller.setFruitFactory(new FruitFactory());
+
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
