5bb1bf5932e0d6541ec7404e54d0e57a0064afe5
SuperAlexcom <fomin_a_p@mail.ru>
2023-05-26 19:59:47 +0300

    Fruit innit commit

---
+++ src/FruitFactory.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class FruitFactory {
+    private static Point DIMENSIONS;
+
+    public static void setDimensions(Point dimensions) {
+        DIMENSIONS = new Point(dimensions);
+    }
+
+    ArrayList<Fruit> createFruits(int count){
+
+    }
+
+}
