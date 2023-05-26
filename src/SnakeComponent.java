c14360ec1b4bfb0e391509fd21bb03cca8ba5735
Artem Pasechnik <pasechnik@1060.ru>
2023-05-26 19:44:59 +0300

    Moved javadoc, added getTail method

--- src/SnakeComponent.java
+++ src/SnakeComponent.java
@@ -1,7 +1,18 @@
 public interface SnakeComponent {
-    void move(Direction direction);
+    /**
+     * Moves whole snake to direction given
+     * @param newDirection Direction to move towards
+     */
+    void move(Direction newDirection);
     boolean checkCollision();
     boolean checkCollision(Point point);
+    /**
+     * Add new tail in the end of snake (MOVE REQUIRED before spawning new tail)
+     */
     void addTail();
+    /**
+     * @return SnakeComponent or null if ended
+     */
+    SnakeComponent getTail();
     int getSize();
 }
